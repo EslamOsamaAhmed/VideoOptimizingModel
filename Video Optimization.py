95745865996e486abde7a4d9544d9bb226484c20
Heba Gamal El-Din <hebagamalfcih@gmail.com>
2020-01-23 16:31:51 -0800

    Update Video Optimization.py

--- Video Optimization.py
+++ Video Optimization.py
@@ -15,7 +15,7 @@ class video_optimize:
     def __init__(self, input_file):
         self.video_in = input_file
         #Standardizing  output Video Format
-        self.video_out = input_file.rsplit('.',1)[0]  + 'out.MP4'
+        self.video_out = input_file.rsplit('.',1)[0]  + 'out35.MP4'
     # get resolution of the input video file
     def findVideoResolution(self):
         cmd = "ffprobe -v error -print_format json -show_streams"
@@ -25,9 +25,9 @@ class video_optimize:
         ffprobeOutput = subprocess.check_output(args).decode('utf-8')
         ffprobeOutput = json.loads(ffprobeOutput)
         # find height and width
-        precent = 80
-        height = ffprobeOutput['streams'][0]['height']   * precent/100
-        width = ffprobeOutput['streams'][0]['width']   * precent/100
+        precent = 75
+        height = int(ffprobeOutput['streams'][0]['height']   * precent/100)
+        width = int(ffprobeOutput['streams'][0]['width']   * precent/100)
         print(width, height)
         #Return Optimized  Video Resolution
         return width , height
@@ -36,7 +36,7 @@ class video_optimize:
         out_width, out_height = self.findVideoResolution()
         resolution =  str(out_width) + ':'  + str(out_height)
         input_fit  = {self.video_in:None}
-        output_fit = {self.video_out:'-n  -preset ultrafast -vcodec libx265  -maxrate 1M  -bufsize 2M -crf 28 -vf  "scale= %s, setdar=9/12" ' %resolution}
+        output_fit = {self.video_out:'-n  -preset ultrafast -vcodec libx265  -maxrate 1M  -bufsize 2M -crf 28 -vf  "scale= %s, setdar=9/15" ' %resolution}
         model = FFmpeg(inputs = input_fit, outputs= output_fit)
         print ('The command used for encoding this video is :: ', model.cmd)
         model.run()
